86edafb6c21657236b7e454c726cfdcb2d910953
paavana-26 <1ms24cs410@msrit.edu>
2026-02-16 12:38:53 +0530

    Create stringUtils.test.js

---
+++ stringUtils.test.js
@@ -0,0 +1,11 @@
+  ```javascript
+   const { capitalize, reverse } = require('./stringUtils');
+   
+   test('capitalize first letter', () => {
+       expect(capitalize('hello')).toBe('Hello');
+   });
+   
+   test('reverse string', () => {
+       expect(reverse('hello')).toBe('olleh');
+   });
+   ```
